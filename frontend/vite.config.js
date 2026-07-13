eb9b256da54d366ba9f194714bc5e38a9a1ea505
ShivD7 <shiv_desai@outlook.com>
2026-07-13 19:57:18 -0400

    bug fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: ProcessingInstruction.env.VITE_BASE_PATH || "/TripProvider"
+  base: process.env.VITE_BASE_PATH || "/",
 });
